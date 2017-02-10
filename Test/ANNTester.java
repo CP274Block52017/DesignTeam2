b4ad984b449a02e7a99c178e5f32016484270056
Callum <neeson.callum@gmail.com>
2017-02-10 14:38:14 -0700

    Commented out tests

--- Test/ANNTester.java
+++ Test/ANNTester.java
@@ -9,7 +9,8 @@ import NeuralNetwork.dataFormatter;
 import NeuralNetwork.testPerceptron;
 
 public class ANNTester {
-
+	
+	//tests functionality of dataFormatter. Makes sure separates into two sets
 	@Test
 	public void dataParsing() {
 		dataFormatter d = new dataFormatter();
@@ -17,7 +18,7 @@ public class ANNTester {
     	DataSet[] trainingAndTestSet = d.getTrainingandTest(normalizedSet, 70, 30);
     	assertTrue(trainingAndTestSet.length == 2);
 	}
-	//this is a test... but is it?
+	//tests functionality of dataFormatter. Makes sure separates into two one larger and one smaller set
 	@Test
 	public void dataParsingSeperateSizes() {
 		dataFormatter d = new dataFormatter();
@@ -25,6 +26,7 @@ public class ANNTester {
     	DataSet[] trainingAndTestSet = d.getTrainingandTest(normalizedSet, 70, 30);
     	assertTrue(trainingAndTestSet[0].size() > trainingAndTestSet[1].size());
 	}
+	//makes sure ANN doesn't crash!!
 	@Test
 	public void runANN() {
 		dataFormatter d = new dataFormatter();
@@ -35,6 +37,7 @@ public class ANNTester {
 		p.print();
 		assertTrue(true); //it doesn't crash!!
 	}
+	//makes sure percent correct is in proper range
 	@Test
 	public void getPercentCorrect() {
 		dataFormatter d = new dataFormatter();
@@ -45,6 +48,7 @@ public class ANNTester {
 		double percentCorrect = p.getPercentCorrect();
 		assertTrue((percentCorrect > 0) && (percentCorrect < 100.01)); 
 	}
+	//makes sure percent correct is over 50
 	@Test
 	public void accuracyOver50() {
 		dataFormatter d = new dataFormatter();
@@ -54,6 +58,7 @@ public class ANNTester {
 		p.run(trainingAndTestSet[0], trainingAndTestSet[1], 30, 2);
 		assertTrue(p.getPercentCorrect()>50); 
 	}
+	//makes sure percent correct is over 65. Working adequately.
 	@Test
 	public void accuracyOver65() {
 		dataFormatter d = new dataFormatter();
