47d11c783473d5148963bce3f1ffdccc077e5f86
SunnyChiu <SunnyChiu@172.16.18.12>
2017-02-12 15:07:41 -0700

    Upload Jar

---
+++ Test/JSONCoversionTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+import DataManipulation.DataObjectInterfaceClasses.DataObject;
+import DataManipulation.DataObjectInterfaceClasses.DateStringObject;
+import preprocessing.DayWordCount;
+import preprocessing.JSONCoverter;
+import preprocessing.WordCount;
+
+public class JSONCoversionTest {
+
+	@Test
+	public void test() {
+		JSONCoverter conversion = new JSONCoverter();
+		List<DayWordCount> file = new ArrayList<DayWordCount>();
+		java.sql.Date date = java.sql.Date.valueOf("2016-01-07");
+
+		for(int t = 0; t < 3; t++){
+			WordCount[] wordcountList = new WordCount[5];
+			for(int i = 0; i < wordcountList.length; i++){
+				
+				WordCount wordCount = new WordCount("tester" + i+t, i);
+				wordcountList[i] = wordCount; 
+			}
+			DayWordCount dateWordCount = new DayWordCount(date, wordcountList);
+			file.add(dateWordCount);
+		}
+		
+		List<DataObject> convertedList = new ArrayList<DataObject>();
+		convertedList = conversion.getJSON(file);
+		for(int i = 0; i < convertedList.size(); i++){
+			date = convertedList.get(i).getDate();
+			String wordListday = ((DateStringObject) convertedList.get(i)).getString();
+			System.out.println(date + ", " +wordListday);
+		}
+		assertTrue(true);
+	}
+
+}
--- src/preprocessing/JSONCoverter.java
+++ src/preprocessing/JSONCoverter.java
@@ -9,11 +9,14 @@ import DataManipulation.DataObjectInterfaceClasses.DateStringObject;
 
 public class JSONCoverter {
 	public List<DataObject> getJSON (List<DayWordCount> file){
-		/*
-		 * 
-		 * 		JSONObject json = new JSONObject();
-		json.put("name", "student");
-
+		
+		  
+ 		JSONObject json1 = new JSONObject();
+ 		json1.put("name", "student");
+ 		json1.put("name", "student2");
+		//for(int j = 0; j < 4; j++){
+			//json1.put("name", "student");
+		//}
 		JSONArray array = new JSONArray();
 		JSONObject item = new JSONObject();
 		item.put("information", "test");
@@ -21,25 +24,28 @@ public class JSONCoverter {
 		item.put("name", "course1");
 		array.put(item);
 
-		json.put("course", array);
+		json1.put("course", array);
 
-		message = json.toString();
+		String message = json1.toString();
+		System.out.println(message);
 		
 		// message
 		// {"course":[{"id":3,"information":"test","name":"course1"}],"name":"student"}*/
 		List<DataObject> returnSet = new ArrayList<DataObject>();
-
+		JSONObject json = new JSONObject();
 		for(int i = 0; i < file.size(); i ++){
 			java.sql.Date sqlDate = file.get(0).getDate();
 			WordCount [] wordlist = file.get(1).getWordCountArray();
 			
-			JSONObject json = new JSONObject();
 			for(int j = 0; j < wordlist.length; j++){
 				String word = wordlist[i].getWord();
 				int count = wordlist[i].getWordCount();
 				json.put(word, count);
+				String jsonString = json.toString();
+				System.out.println(jsonString);
 			}
 			String jsonString = json.toString();
+			System.out.println(jsonString);
 			DateStringObject rowObject = new DateStringObject(sqlDate,jsonString);
 			returnSet.add(rowObject);
 		}
