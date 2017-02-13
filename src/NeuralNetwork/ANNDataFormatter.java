e223ff04350f6105828a946c8a3fb13d14eb188f
Callum <neeson.callum@gmail.com>
2017-02-12 22:12:24 -0700

    Testing expanded. ANNData Formatter Complete(For now).

--- src/NeuralNetwork/ANNDataFormatter.java
+++ src/NeuralNetwork/ANNDataFormatter.java
@@ -13,18 +13,29 @@ public class ANNDataFormatter {
 	public ANNDataFormatter() {}
 	
 	public DataSet toDataSet(ArrayList<int[]> set) {
-		int OUTPUT_SIZE = 16;
-		DataSet d = new DataSet(set.get(0).length - OUTPUT_SIZE, OUTPUT_SIZE );
+		int OUTPUT_SIZE = 1;
+		int BINARY_SIZE = 16;
+		DataSet d = new DataSet(set.get(0).length - OUTPUT_SIZE, OUTPUT_SIZE * BINARY_SIZE);
 		for(int[] currentRow : set) {
-			int[] input = Arrays.copyOfRange(currentRow, OUTPUT_SIZE,currentRow.length );
+			int[] input = Arrays.copyOfRange(currentRow, 1,currentRow.length );
 			double[] dbleInput = Arrays.stream(input).asDoubleStream().toArray();
-			int[] output = Arrays.copyOfRange(currentRow, 0, OUTPUT_SIZE);
-			double[] dbleOutput = Arrays.stream(output).asDoubleStream().toArray();
-			d.addRow(dbleInput, dbleOutput);
+			double[] output = outputToBinary(currentRow[0]);
+			d.addRow(dbleInput, output);
 		}
 		return d;
-		
-		
 	}
+	
+	private double[] outputToBinary(int nonBinary ) {
+		String binary = Integer.toBinaryString(nonBinary);
+		double[] output = new double[16];
+		for(int i = 0; i < binary.length(); i++) {
+			output[i+(16-binary.length())] = Character.getNumericValue(binary.charAt(i));
+		}
+		for(int j = 0;j<16-binary.length();j++){
+			output[j] = 0;
+		}
+		return output;
+	}
+	
 
 }
