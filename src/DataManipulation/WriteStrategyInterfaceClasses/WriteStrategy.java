f7124136a4ccf199705a401e7241dd0aeb6667ce
Eli <Eli@172.16.16.16>
2017-02-14 15:27:22 -0700

    comment

--- src/DataManipulation/WriteStrategyInterfaceClasses/WriteStrategy.java
+++ src/DataManipulation/WriteStrategyInterfaceClasses/WriteStrategy.java
@@ -10,7 +10,7 @@ import DataManipulation.DataObjectInterfaceClasses.DataObject;
 /**
  * This interface defines the method for writing data objects to their
  * corresponding tables in the database.
- *
+ * comment
  */
 public interface WriteStrategy {
 	void writeToTable(List<DataObject> file, Connection databaseConnection) throws SQLException, ParseException;
